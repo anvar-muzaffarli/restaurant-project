22c24f99344a22ce5c4c17c5efa09ff163e17b13
anvar-muzaffarli <prof.anvar2013@gmail.com>
2026-02-07 19:57:31 +0400

    7 Fevral tarixli dərs

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,68 @@ closeModalBtn.addEventListener("click", ()=> {
 
 
 
+
+
+// YENI MOVZU - XUSUSI OLARAQ SIZDEN SORUSULACAQ
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelector("#menuTabs")
+    // console.log(tabs)
+    const cards = document.querySelectorAll(".pizza-card")
+
+    // querySelectorAll NodeList qaytarir vs getElementsByClassName HTMLCollection 
+// NodeList qaytardigi coxluqda massive aid olan metodlari istifade ede bilirem. for, forEach, for of, map, filter, find
+    // true ve ya false = 1
+    //true + true = true
+    // true + false = true (|| ve ya  or)
+    if(!tabs ||  cards.length === 0) return;
+
+    // function expression izah ele
+    const setActiveTab = (activeBtn) => {
+        const allBtns = document.querySelectorAll(".tab-btn")
+        // harada cemlik varsa orada [] var. Harada [] varsa, onu oxutmaq ucun for ve ya onun toremeleri (forEach, map)
+        // HOC Higher ORder Function
+        // callback
+        allBtns.forEach((btn)=> {
+            const isActive = btn === activeBtn //is varsa bunun iki cavabi var ya true, ya da false
+            // DOMa aid olan metodlardir
+            btn.classList.toggle("active-selection", isActive)
+// true ya da false "true" "false"
+            btn.setAttribute("aria-selected", String(isActive))
+
+        })
+    }
+// meat, vegetarian
+    const applyFilter = (filter) => {
+        cards.forEach(card => {
+            // string metodlarindadir. Arashdirin string metodlarini ve massiv method (pop, push)
+            // RegEx regular expression duzernli ifade
+            // const emailQaydasi = /^\s $/
+
+            // mushroom sea  => ["mushroom","sea"]
+
+           
+            const cats = (card.dataset.category || "").toLowerCase().split(/\s+/)
+            const show = filter === "all" || cats.includes(filter)
+
+            card.classList.toggle("hidden", !show)
+        })
+    }
+
+    tabs.addEventListener("click", (e)=> {
+        const btn = e.target.closest(".tab-btn")
+        if(!btn) return;
+
+        const filter = (btn.dataset.filter || "all").toLowerCase()
+
+        setActiveTab(btn)
+        applyFilter(filter)
+    })
+
+
+   
+})
+
